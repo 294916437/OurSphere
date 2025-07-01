c8ad1ca317fc313a4b7ed3388f9d0ced78bc9cde
294916437 <du2959346375@outlook.com>
2025-07-01 10:06:24 +0800

    feat(mybatis):add mybatis module for user entity

---
+++ oursphere-auth/src/main/java/com/theoyu/oursphere/auth/domain/dataobject/UserDo.java
@@ -0,0 +1,25 @@
+package com.theoyu.oursphere.auth.domain.dataobject;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDo {
+
+    private Long id;
+
+    private String username;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+}
+
