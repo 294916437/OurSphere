9b87056eb039254efe3595b809297c9f93f02857
294916437 <du2959346375@outlook.com>
2025-07-04 21:39:36 +0800

    feat(redisTemplate):add redisTemplate and relevant configs

--- oursphere-auth/src/main/java/com/theoyu/oursphere/auth/controller/TestController.java
+++ oursphere-auth/src/main/java/com/theoyu/oursphere/auth/controller/TestController.java
@@ -1,26 +1,28 @@
 package com.theoyu.oursphere.auth.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.theoyu.framework.logger.aspect.ApiOperationLog;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.theoyu.framework.common.response.Response;
 import java.time.LocalDateTime;
 
-
 @RestController
 public class TestController {
-    @GetMapping("/test")
-    @ApiOperationLog(description = "测试接口")
-    public Response<String> test() {
-        return Response.success("Hello,World!");
+    // 测试登录，浏览器访问： http://localhost:8080/user/doLogin?username=zhang&password=123456
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
     }
 
-    @PostMapping("/test2")
-    @ApiOperationLog(description = "测试接口2")
-    public Response<User> test2(@RequestBody User user) {
-        int i = 1 / 0; // 故意制造一个异常
-        return Response.success(user);
+    // 查询登录状态，浏览器访问： http://localhost:8080/user/isLogin
+    @RequestMapping("/user/isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
     }
+
 }
