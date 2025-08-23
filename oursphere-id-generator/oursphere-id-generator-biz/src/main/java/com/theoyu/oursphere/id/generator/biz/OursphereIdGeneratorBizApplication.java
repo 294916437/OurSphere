6cd5e32230815b1cec4a7a6bd7fddf084350409d
294916437 <du2959346375@outlook.com>
2025-08-23 14:36:29 +0800

    feat(id-generator):realize the distributed id-generator with MeiTuan-Dianping-leaf method

---
+++ oursphere-id-generator/oursphere-id-generator-biz/src/main/java/com/theoyu/oursphere/id/generator/biz/OursphereIdGeneratorBizApplication.java
@@ -0,0 +1,13 @@
+package com.theoyu.oursphere.id.generator.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OursphereIdGeneratorBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OursphereIdGeneratorBizApplication.class, args);
+    }
+
+}
---
+++ oursphere-id-generator/oursphere-id-generator-biz/src/main/java/com/theoyu/oursphere/id/generator/biz/core/common/Status.java
@@ -0,0 +1,6 @@
+package com.theoyu.oursphere.id.generator.biz.core.common;
+
+public enum  Status {
+    SUCCESS,
+    EXCEPTION
+}
