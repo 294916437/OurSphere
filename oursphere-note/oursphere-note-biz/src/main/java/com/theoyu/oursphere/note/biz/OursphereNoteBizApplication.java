653f5648406b2d58b5d63295ae4ec405d014da62
294916437 <du2959346375@outlook.com>
2025-08-23 15:40:39 +0800

    feat(note):add config of note microservice

---
+++ oursphere-note/oursphere-note-biz/src/main/java/com/theoyu/oursphere/note/biz/OursphereNoteBizApplication.java
@@ -0,0 +1,13 @@
+package com.theoyu.oursphere.note.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OursphereNoteBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OursphereNoteBizApplication.class, args);
+    }
+
+}
