da054447f94f6095291d3fc4d9bad6e5daf9c52c
294916437 <du2959346375@outlook.com>
2025-09-05 21:44:27 +0800

    refactor(search-note):optimize the note search with keyword hot update

---
+++ oursphere-search/src/main/java/com/theoyu/oursphere/search/service/ExtDictService.java
@@ -0,0 +1,11 @@
+package com.theoyu.oursphere.search.service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface  ExtDictService {
+    /**
+     * 获取热更新词典
+     * @return
+     */
+    ResponseEntity<String> getHotUpdateExtDict();
+}
