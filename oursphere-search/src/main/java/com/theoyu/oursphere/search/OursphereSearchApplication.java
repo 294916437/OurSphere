4b25b27db356f6d9c38ad44f2fd542bc56c5cfcb
294916437 <du2959346375@outlook.com>
2025-09-06 20:57:55 +0800

    feat(canal):realize the canal client

--- oursphere-search/src/main/java/com/theoyu/oursphere/search/OursphereSearchApplication.java
+++ oursphere-search/src/main/java/com/theoyu/oursphere/search/OursphereSearchApplication.java
@@ -2,8 +2,10 @@ package com.theoyu.oursphere.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OursphereSearchApplication {
 
     public static void main(String[] args) {
